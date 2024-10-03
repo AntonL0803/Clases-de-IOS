016a0fa627c4dbb9f6735f425dd180f7d379c5f8
Usuario invitado <Guest@B213-18.local>
2024-10-03 16:45:44 +0200

    hola

---
+++ ClasesDAM/main.swift
@@ -0,0 +1,26 @@
+//
+//  main.swift
+//  ClasesDAM
+//
+//  Created by Usuario invitado on 3/10/24.
+//
+
+import Foundation
+
+//print("Hello, World!")
+
+//print("Introduce tu nombre: ")
+//let nombre: String? = readLine()
+//print("Hola " + nombre!)
+//print("Hola \(nombre!)")
+
+print("dame dos numeros: ")
+let numero1 = Int(readLine()!)!
+let numero2 = Int(readLine()!)!
+
+    if (numero1 > numero2) {
+        print("\(numero1) es mayor que el \(numero2)")
+    } else {
+        print("\(numero2) e mayor que el \(numero1)")
+    }
+        
